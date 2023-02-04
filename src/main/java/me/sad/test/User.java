fec289b081e6bf3215f32ab07f77662c14d96393
olensis <olensis7@gmail.com>
2023-02-04 18:01:11 +0300

    готово

--- src/main/java/me/sad/test/User.java
+++ src/main/java/me/sad/test/User.java
@@ -1,4 +1,33 @@
 package me.sad.test;
 
 public class User {
+    private String login;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String login, String email) {
+        if (!login.equals(email)) {
+            this.login = login;
+        } else {
+            throw new RuntimeException();
+        }
+
+        if (email.contains("@") && email.contains(".")) {
+            this.email = email;
+        } else {
+            throw new RuntimeException();
+        }
+
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
---
+++ src/test/java/me/sad/test/UserTest.java
@@ -0,0 +1,41 @@
+package me.sad.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    @Test
+    @DisplayName("When User is created with login and email then there parameters are installed correct")
+    public void createUser() {
+        String login = "sad";
+        String email = "sad@mail.ru";
+        User user = new User(login, email);
+        Assertions.assertEquals(login, user.getLogin());
+        Assertions.assertEquals(email, user.getEmail());
+    }
+
+    @Test
+    @DisplayName("When Users Constructor is empty , then login and email are null")
+    public void createISEmpty() {
+        User user = new User();
+        Assertions.assertNull(user.getEmail());
+        Assertions.assertNull(user.getLogin());
+    }
+
+    @Test
+    @DisplayName("When email  has not  @ and ' . ' , then email is not correct ")
+    public void correctEmailException() {
+        String email = "sad@ru";
+        String login = "sad";
+        Assertions.assertThrows(RuntimeException.class,()-> new User(login,email) );
+    }
+
+    @Test
+    @DisplayName("When login is not equals email , then parameters are correct")
+    public void loginNotEqualsEmail() {
+        String login = "sad";
+        String email = "sad@ru";
+        Assertions.assertThrows(RuntimeException.class,()-> new User(login,email));
+    }
+}
